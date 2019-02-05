446bb7698c4f6601cfe2b8a8f602181557690a3d
Marieme ka <45308240+mariemeka@users.noreply.github.com>
2019-02-05 13:26:52 +0100

    tableau en php

--- Mon_tableau/tableau.php
+++ Mon_tableau/tableau.php
@@ -1,11 +1,30 @@
+<!doctype html>
+<html>
+<head>
+<title> tableau </title>
+</br>
+<meta charset="utf-8">
+<link rel="stylesheet" href="css.css">
+</head>
+<body>
+<center><h1> tableau</h1></center>
+<center><table border="1" width=30%  >
+<td>
+<center> <img src="photo.jpg" width="200" height="205" align="center"> </br></br></center><hr>
+
 <?php
-$etudiants=array("samba", "abdou", "fatou");
-echo $etudiants[2];
-
-for($i=0; $i<=count($etudiants); $i++){
-echo $etudiants[$i]."<br>";
+$personne=array(
+"NOM    :"=>"KA",
+"PRENOM  :"=>"MARIEME",
+"NUMERO  :"=>"777777745",
+"EMAIL   :"=>"marieme1212@gmail.com",
+"ADRESSE     :"=>"hlm grand yoff",
+"ID_SLACK     :"=>"marieme1212",
+"ID_GITHUB  :"=>"mariemeka",
+);
+foreach($personne as $key =>$value){
+echo $key.   '   ' .$value.  '<br/>';
 }
-
-
-
 ?>
+</td>
+</table></center>
